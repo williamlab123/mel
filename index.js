65ff5e78e075dabd37c9076f863459b11cef4983
William Pedro <54008790+williamlab123@users.noreply.github.com>
2023-02-04 13:05:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 
 
-document.addEventListener("mousemove", function(event) {
-    if (event.clientX > window.innerWidth - 200) {
-      document.getElementById("side-menu").classList.add("open");
-    } else {
-      document.getElementById("side-menu").classList.remove("open");
+// document.addEventListener("mousemove", function(event) {
+//     if (event.clientX > window.innerWidth - 200) {
+//       document.getElementById("side-menu").classList.add("open");
+//     } else {
+//       document.getElementById("side-menu").classList.remove("open");
+//     }
+//   });
+
+  const images = ['imgs/Screenshot_1.png', 'imgs/Screenshot_2.png', 'imgs/Screenshot_3.png'];
+  const currentSlide = document.querySelector('#current-slide');
+  let currentIndex = 0;
+  
+  function prevSlide() {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
     }
-  });
+    currentSlide.src = images[currentIndex];
+  }
+  
+  function nextSlide() {
+    currentIndex++;
+    if (currentIndex === images.length) {
+      currentIndex = 0;
+    }
+    currentSlide.src = images[currentIndex];
+  }
+
+  document.addEventListener("mousemove", function(event) {
+if (event.clientX > window.innerWidth - 250) {
+document.getElementById("side-menu").classList.add("open");
+} else {
+document.getElementById("side-menu").classList.remove("open");
+}
+});
 
 //   $(document).ready(function() {
 //     $('.slider').slick({
